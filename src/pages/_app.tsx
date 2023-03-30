cf395621cf2f0ce8d8297dabbc452752e163d4fe
JeanVictor44 <jeanvictorgithubprojects@gmail.com>
2023-03-30 16:37:32 -0300

    feat: studying default next folders and files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700', '900'],
+  style: ['normal'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family:${roboto.style.fontFamily};
+        }
+      `}</style>
+      <h1>HEader</h1>
+      <Component {...pageProps} />
+      
+    </>
+    
+  )
+
 }
